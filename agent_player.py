44a5b45306b25032c21319bcc4c78703c4512e26
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2025-03-27 02:16:13 -0600

    Adding agent logic

--- agent_player.py
+++ agent_player.py
@@ -8,19 +8,57 @@ This player can:
 '''
 
 import numpy as np
+from math import inf
+import pickle
 
 class AgentPlayer:
-    def __init__(self, name, epsilon=1):
+    def __init__(self, name, epsilon=0):
         self.name = name
         self.states = []    # All positions taken during each game
         self.epsilon = epsilon
-        self.gamma = 0.9    
+        self.gamma = 0.9   
+        self.lr = 0.2
         self.state_values = {}
 
     def choose_action(self, env):
         actions = env.get_actions()
+        player = env._player
 
         if np.random.uniform(0, 1) <= self.epsilon:
             action = np.random.choice(actions)
-            
-            return action
+
+        else:
+            max_value = -inf
+            for a in actions:
+                next_env = env.clone()
+                next_env.step(a)
+                board = next_env.get_observation(player)
+
+                if self.state_values.get(board) is None:
+                    value = 0
+                else:
+                    value = self.state_values[board]
+
+                if value > max_value:
+                    max_value = value
+                    action = a
+
+        return action
+    
+    def feedReward(self, reward):
+        for state in reversed(self.states):
+            if self.state_values.get(state) is None:
+                self.state_values[state] = 0
+            self.state_values[state] += self.lr * (self.gamma * reward - self.state_values[state])
+            reward = self.state_values[state]
+
+    def savePolicy(self):
+        file_w = open('policy.pkl', 'wb')
+        pickle.dump(self.state_values, file_w)
+        file_w.close()
+
+    def loadPolicy(self):
+        file_r = open('policy.pkl', 'rb')
+        self.state_values = pickle.load(file_r)
+        file_r.close()
+        print('Policy loaded')
